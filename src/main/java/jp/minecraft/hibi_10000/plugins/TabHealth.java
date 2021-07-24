d841e744d2b305945c4de6ce0b61b3d31b3c816f
Hibi-10000 <87810571+Hibi-10000@users.noreply.github.com>
2021-07-25 06:51:42 +0900

    Add files via upload

---
+++ src/main/java/jp/minecraft/hibi_10000/plugins/TabHealth.java
@@ -0,0 +1,44 @@
+package jp.minecraft.hibi_10000.plugins;
+
+import java.text.NumberFormat;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+public class TabHealth extends JavaPlugin {
+	
+	BukkitTask task = null;
+	private static NumberFormat numb = NumberFormat.getInstance();
+	
+	static {
+		numb.setMaximumFractionDigits(1);
+	}
+    
+	public void onEnable() {
+		super.onEnable();
+		task = new BukkitRunnable() {
+			public void run() {
+				for(Player player : Bukkit.getOnlinePlayers()) {
+					String health = numb.format(player.getHealth());
+					String color;
+					if(player.getHealth() <= 6) {
+						color = "§C§L";
+					} else if(player.getHealth() <= 12) {
+						color = "§E§L";
+					} else {
+						color = "§A§L";
+					}
+					String name = color + player + " :" + health;
+					player.setPlayerListName(name);
+				}
+			}
+		}.runTaskTimer(this,0L, 20L);
+	}
+	
+	public void onDisable() {
+		super.onDisable();
+	}
+}
