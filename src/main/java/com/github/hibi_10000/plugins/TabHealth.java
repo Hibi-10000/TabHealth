960f23412f3922959bef19f6fa9585c3807052f6
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2024-04-17 18:19:32 +0900

    fix: コードを改善

--- src/main/java/com/github/hibi_10000/plugins/TabHealth.java
+++ src/main/java/com/github/hibi_10000/plugins/TabHealth.java
@@ -3,43 +3,33 @@ package com.github.hibi_10000.plugins;
 import java.text.NumberFormat;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
 public class TabHealth extends JavaPlugin {
-	
-	BukkitTask task = null;
-	private static NumberFormat numb = NumberFormat.getInstance();
-	
-	static {
-		numb.setMaximumFractionDigits(1);
-	}
-    
-	public void onEnable() {
-		super.onEnable();
-		task = new BukkitRunnable() {
-			public void run() {
-				for(Player player : Bukkit.getOnlinePlayers()) {
-					String health = numb.format(player.getHealth());
-					String color;
-					String p = player.getName();
-					if(player.getHealth() <= 6) {
-						color = "§C§L";
-					} else if(player.getHealth() <= 12) {
-						color = "§E§L";
-					} else {
-						color = "§A§L";
-					}
-					String name = color + p + " :" + health;
-					player.setPlayerListName(name);
-				}
-			}
-		}.runTaskTimer(this,0L, 20L);
-	}
-	
-	public void onDisable() {
-		super.onDisable();
-	}
+    BukkitTask task = null;
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        final NumberFormat numb = NumberFormat.getInstance();
+        numb.setMaximumFractionDigits(1);
+        task = getServer().getScheduler().runTaskTimer(this, () -> {
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                String health = numb.format(p.getHealth());
+                ChatColor color;
+                if (p.getHealth() <= 6) {
+                    color = ChatColor.RED;
+                } else if (p.getHealth() <= 12) {
+                    color = ChatColor.YELLOW;
+                } else {
+                    color = ChatColor.GREEN;
+                }
+                String name = color.toString() + ChatColor.BOLD + p.getName() + " :" + health;
+                p.setPlayerListName(name);
+            }
+        }, 0L, 20L);
+    }
 }
